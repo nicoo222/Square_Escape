f668b3ed0f98def5d7865cd532b623d2d3ca9571
Pons Jacques <ich@mypc.haus>
2015-05-16 21:28:47 +0200

    modif du main

--- main.c
+++ main.c
@@ -14,59 +14,60 @@ int main(int argc, char *argv[])
 	audioInitilization();
 	
 	int frameTime, currentTime, frame = 0, delay;
+	// durée de la boucle de jeu en ms
+	// 17 correspond envirron à 60 fps
+	int frameDuration = 17;
 	
 	memset(&gameState,0,sizeof(gameState));
 	memset(&in,0,sizeof(in));
 	
 	while(!in.quit){
 
-		//Si le retour de boucle s'effectue dans le cas d'une nouvelle partie ou si le joueur a décidé de rejouer
-		if(!gameState.pause){
-			characterInitialization(&character,&screen);
-			enemiesInitialization(&enemies,&screen);
-			updateScreen(&character,&enemies,&screen);
-		}
-		
 		//Mise à 0 des des booléens des structures.
 		memset(&in,0,sizeof(in));
 		memset(&gameState,0,sizeof(gameState));
 		
-		while(!gameState.pause && !in.quit && !gameState.lost){
-		    frameTime = SDL_GetTicks();
-		    frame++;
-		    moveEnemies(&enemies);
-		    updateInput(&in);
-		    checkCollision(&character,&enemies,&collision,&gameState);
-		    updateScreen(&character,&enemies,&screen);
-		    
+		characterInitialization(&character,&screen);
+		enemiesInitialization(&enemies,&screen);
+		
+		updateScreen(&character,&enemies,&screen);
+		
+		
+		
+		while(!in.keys[SDL_SCANCODE_P] && !in.quit && !gameState.lost){
+			frameTime = SDL_GetTicks();
+			frame++;
+			
+			//on ajoute un ennemy régulièrement
+			if (frame%120 == 0){
+				addOneEnemy(&enemies);
+			}
+			
+			updateInput(&in);
+			
 			if (in.keys[SDL_SCANCODE_UP]){
-		 	  	if(!collision.up){
-		  			character.squarePosition.y-=6;
-			 	}
+				if(!collision.up){
+					character.squarePosition.y-=6;
+				}
 			}
 		
 			if (in.keys[SDL_SCANCODE_LEFT]){
 				if(!collision.left){
-			  		character.squarePosition.x-=6;
-		  		}
-			  		
+					character.squarePosition.x-=6;
+				}
+					
 			}
 
 			if (in.keys[SDL_SCANCODE_DOWN]){
-			  	if(!collision.down){
-			  		character.squarePosition.y+=6;
-		  		}
+				if(!collision.down){
+					character.squarePosition.y+=6;
+				}
 			}
 
 			if(in.keys[SDL_SCANCODE_RIGHT]){
 			  if(!collision.right){
-			  		character.squarePosition.x+=6;
-		  		}
-			}
-		
-			if(in.keys[SDL_SCANCODE_P]){
-				gameState.pause=1;
-				in.keys[SDL_SCANCODE_P]=0;
+					character.squarePosition.x+=6;
+				}
 			}
 			
 			if(in.keys[SDL_SCANCODE_SPACE]){
@@ -74,39 +75,33 @@ int main(int argc, char *argv[])
 				in.keys[SDL_SCANCODE_SPACE]=0;
 			}
 			
-			currentTime = SDL_GetTicks();
 			
-			delay = 4 - currentTime + frameTime;
+			checkCollision(&character,&enemies,&collision,&gameState);
+			moveEnemies(&enemies);
+			updateScreen(&character,&enemies,&screen);
+			
+			//code pour attendre un durée de frame fixe
+			currentTime = SDL_GetTicks();
+			delay = frameDuration - currentTime + frameTime;
 			if (delay > 0) {
-				SDL_Delay(4 - currentTime + frameTime);
+				SDL_Delay(frameDuration - currentTime + frameTime);
 			}
 		}
 		
 		if(!in.quit){
 			gameState.waiting=1;
 			
-			if(gameState.lost){
-				//On attend que l'utilisateur décide de rejouer ou non
-				while(gameState.waiting && !in.quit){
-					updateInput(&in);
+			//On attend que l'utilisateur décide de rejouer ou non
+			while(gameState.waiting && !in.quit){
+				updateInput(&in);
 				
-					if(in.keys[SDL_SCANCODE_N]){
-						in.quit=1;
-						gameState.waiting=0;
-					}
-				
-					if(in.keys[SDL_SCANCODE_Y]){
-						gameState.waiting=0;
-					}
+				if(in.keys[SDL_SCANCODE_N]){
+					in.quit=1;
+					gameState.waiting=0;
 				}
-			}
-			else {
-				while(gameState.waiting && !in.quit){
-					updateInput(&in);
-					
-					if(in.keys[SDL_SCANCODE_P]){
-						gameState.waiting=0;
-					}
+				
+				if(in.keys[SDL_SCANCODE_Y]){
+					gameState.waiting=0;
 				}
 			}
 		}
